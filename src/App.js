09b6e0035761b2a9750b128f60da8f48b7516e36
Eric <outboundflavor@gmail.com>
2020-03-17 15:35:32 -0500

    removed app conflicts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
-import Home from './component/Home.js'
+import React, {Component} from 'react';
 
 
 class App extends Component {
+
   render() {
     return (
       <div className="container">
-        <Home />
+
+
+
       </div>
     )
   }
