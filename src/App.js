ed982d0a2e5c9c474c7c511a3e9c1db857ead990
Eric <outboundflavor@gmail.com>
2020-03-17 14:32:04 -0500

    added static divs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import Home from './component/Home.js'
 
 
 class App extends Component {
-
   render() {
     return (
       <div className="container">
-
+        <Home />
 
 
       </div>
---
+++ src/component/home.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Boots Site</h1>
+        <div>
+          <div>
+            <div>INDOOR</div>
+            <span>SHOP NOW</span>
+          </div>
+        </div>
+        <div>
+          <div>
+              <div>OUTDOOR</div>
+            <span>SHOP NOW</span>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div>FASHION</div>
+            <span>SHOP NOW</span>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div>WOMENS</div>
+            <span>SHOP NOW</span>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div>MENS</div>
+            <span>SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
