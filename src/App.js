93eca9af9f283c36c056d35937b1d3c3d82d88e9
Eric <outboundflavor@gmail.com>
2020-03-18 10:51:08 -0500

    added new page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Cards from './component/Cards.js'
 import Home from './component/Home.js'
 import Cart from './component/Cart.js'
+import New from './component/New.js'
 
 
 class App extends Component {
@@ -15,10 +16,12 @@ class App extends Component {
           <Link to="/">Home</Link>
           <Link to="/shop">Shop</Link>
           <Link to="/cart">Cart</Link>
+          <Link to="/new">New</Link>
         </nav>
         <Route path="/" exact component={Home} />
         <Route path="/shop" component={Cards} />
         <Route path="/cart" component={Cart} />
+        <Route path="/new" component={New} />
         </div>
       </Router>
     )
