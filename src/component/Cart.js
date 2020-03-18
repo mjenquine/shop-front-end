364d10345328fa35ef1517ecd83ec63a26409083
vksharma <vikram.sharma1024@gmail.com>
2020-03-18 11:57:02 -0400

    commit cart populated with API data

--- src/component/Cart.js
+++ src/component/Cart.js
@@ -1,17 +1,37 @@
 import React, {Component} from 'react'
 import CartItem from './CartItem'
-import data from '../testData.js'
+// import data from '../testData.js'
+
+let baseURL = process.env.REACT_APP_BASEURL
+
+if (process.env.NODE_ENV === 'development') {
+  baseURL = 'http://localhost:3003'
+} else {
+  baseURL = 'https://enigmatic-mountain-68507.herokuapp.com/'
+}
 
 class Cart extends Component{
     constructor(props){
         super(props)
         this.state = {
-            items: []
+            cartItems: [],
+            cartItem: {}
+        }
+        this.getItems = this.getItems.bind(this)
+    }
+
+    async getItems(){
+        try{
+            let response = await fetch(`${baseURL}/shop`)
+            let data = await response.json()
+            this.setState({cartItems: data})
+        }catch(e){
+            console.error(e)
         }
     }
 
     componentDidMount (){
-        this.setState({items: data})
+        this.getItems()
     }
 
     render(){
@@ -20,12 +40,9 @@ class Cart extends Component{
                 <table>
                     <tbody>
                     <thead>Item Table:</thead>
-                        {this.state.items.map(item=>{
-                            return(
-                                <CartItem item={item} key={item.id}/>
-                            )
-                        })}
-
+                        {this.state.cartItems.map((item, index) => (
+                            <CartItem cartItem={item} key={item._id}/>
+                        ))}
                     </tbody>
                 </table>
             </div>
--- src/component/CartItem.js
+++ src/component/CartItem.js
@@ -1,18 +1,57 @@
 import React, {Component} from 'react'
+import Cart from './Cart'
+
+let baseURL = process.env.REACT_APP_BASEURL
+
+if (process.env.NODE_ENV === 'development') {
+  baseURL = 'http://localhost:3003'
+} else {
+  baseURL = 'https://enigmatic-mountain-68507.herokuapp.com/'
+}
 
 class CartItem extends Component{
+
+    constructor(state){
+        super(state)
+        this.state = {
+            cartItem: {
+                name: '',
+                price: '',
+                quantity: 0,
+                description: ''
+            }
+        }
+        // this.getItem = this.getItem.bind(this)
+    }
+
+    // componentDidMount(){
+    //     // this.getItem()
+    // }
+
+    // async getItem(){
+    //     try{
+    //         let response = await fetch(`${baseURL}/shop/${this.props.cartItem._id}`)
+    //         let data = await response.json()
+    //         this.setState({item:data})
+    //         console.log(this.props.cartItem)
+    //     } catch(e){
+    //         console.error(e)
+    //     }
+    // }
+
+
     render(){
         return(
-            <tr key={this.props.item.id}>
-                <td>Item: {this.props.item.name}</td>
+            <tr key={this.props.cartItem._id}>
+                <td>Item: {this.props.cartItem.name}</td>
 
-                <td>Price: ${this.props.item.price}</td>
+                <td>Price: ${this.props.cartItem.price}</td>
                 <td>Quantity:
                     <a href="#">-</a>
-                    {this.props.item.quantity}
+                    {this.props.cartItem.quantity}
                     <a href="#">+</a>
                 </td>
-                <td>description: {this.props.item.description}</td>
+                <td>description: {this.props.cartItem.description}</td>
                 <td><button>DELETE</button></td>
             </tr>
         )
