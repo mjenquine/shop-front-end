f80880528e9b9330ba4a975acdeeb60aa07e3849
Eric <outboundflavor@gmail.com>
2020-03-19 10:11:23 -0500

    morning pull

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
     return (
       <Router>
         <div className="container">
+<<<<<<< HEAD
         <nav>
           <h1>Boots</h1>
           <Link to="/">Home</Link>
@@ -19,6 +20,22 @@ class App extends Component {
           <Link to="/cart">Cart</Link>
           <Link to="/sign">Sign Up</Link>
         </nav>
+=======
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/shop">Shop</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/cart">Cart</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/new">New</Link>
+          </li>
+        </ul>
+>>>>>>> 3c92215c7a097417d7ac84863d98ad2d6f6d1ebb
         <Route path="/" exact component={Home} />
         <Route path="/shop" component={Cards} />
         <Route path="/cart" component={Cart} />
